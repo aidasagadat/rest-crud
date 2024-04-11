1798e9de6793efecde9a6a97c2ce222734b542f9
Aida Sagadat <aidasagadat355@gmail.com>
2024-04-11 21:08:57 +0500

    adding @PostConstruct

--- src/main/java/com/aidasagadat/restcrud/rest/StudentRestController.java
+++ src/main/java/com/aidasagadat/restcrud/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.aidasagadat.restcrud.rest;
 
 import com.aidasagadat.restcrud.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,24 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
+    private List<Student> students;
 
-        List<Student> students = new ArrayList<>();
+    // define @PostConstruct to load the student data ... only once!
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
         students.add(new Student("Aida", "Sagadat"));
         students.add(new Student("Adiya", "Sagrsenbaeva"));
         students.add(new Student("Zhansulu", "Yensebaeva"));
+    }
+
+
+    @PostConstruct
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+
+
 
         return students;
     }
